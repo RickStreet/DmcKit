bc74b171e09137fba4a7b107483bde842872eb18
RickStreet <Rick_1@me.com>
2022-06-28 13:55:45 -0700

    Made Gmult Equitable

--- Sources/DmcKit/GMult.swift
+++ Sources/DmcKit/GMult.swift
@@ -8,10 +8,14 @@
 
 import Foundation
 
-public struct GMult {
+public struct GMult: Equatable {
     public var indIndex = 0
     public var depIndex = 0
     public var value = 0.0
+    
+    public static func ==(lhs: GMult, rhs: GMult) -> Bool {
+        return lhs.indIndex == rhs.indIndex && lhs.depIndex == rhs.depIndex && lhs.value == rhs.value
+    }
     
     public init(indIndex: Int, depIndex: Int, value: Double) {
         self.indIndex = indIndex
